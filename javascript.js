29e0b5f3ab6d627e4875b7c70ccac4bd07b2da61
catrionafsmith <113362369+catrionafsmith@users.noreply.github.com>
2022-11-02 22:21:11 +0000

    Saved code for scratch-off kitten pic

--- javascript.js
+++ javascript.js
@@ -17,13 +17,11 @@ function promptMe(){
         box.className = "box";
         
         box.addEventListener('mouseout', () => {
-            // box.className = "hov_col";
-            // let hov_box = document.querySelector(".hov_col");
             box.style.backgroundColor = getColor()
-            // [Math.floor(Math.random*5)]
+            
             
         })
-        container.appendChild(box)
+    container.appendChild(box)
     } 
 
 }
@@ -62,3 +60,41 @@ function getColor() {
 // function getColor() {
 //    return  colour_list[Math.floor(Math.random()*5)]
 // }
+
+
+// SCRATCH OFF VERSION
+
+// 
+// let n = 10;
+// let colour_list = ["#d9d2e9", "#ccbaf5", "#cfe2f3", "#e5cdd8", "#e5adad"];
+// // import random
+
+// function promptMe(){
+//     n = Number(prompt("Type a number to set the number of squares in each side of the Etch-a Sketch (up to 100)"));
+//     let cont_width = (480/n).toString();
+
+//     const container = document.querySelector(".container");
+
+//     container.style.gridTemplateColumns = `repeat(${n}, ${cont_width}px`;
+
+
+//     for (let i= 0; i<(n*n); i++) {
+//         let box = document.createElement('div');
+//         box.className = "box";
+        
+//         box.addEventListener('mouseout', () => {
+//             // box.className = "hov_col";
+//             // let hov_box = document.querySelector(".hov_col");
+//             box.style.opacity = lessOpaque()
+//             // [Math.floor(Math.random*5)]
+            
+//         })
+//         container.appendChild(box)
+//     } 
+
+// }
+// let opacity_value = 1;
+// function lessOpaque() {
+//     opacity_value -= 0.01;
+//     return  opacity_value
+//  }
